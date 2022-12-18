9d71d6c4fd921b29b36d8512c66e285d2dd265b0
skiva1 <User@ASUS-I5>
2022-12-17 21:20:17 -0300

    Added Daddy

--- src/main/java/net/mcreator/unnamed/init/UnnamedModItems.java
+++ src/main/java/net/mcreator/unnamed/init/UnnamedModItems.java
@@ -13,11 +13,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.unnamed.item.DaddyItem;
 import net.mcreator.unnamed.UnnamedMod;
 
 public class UnnamedModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, UnnamedMod.MODID);
 	public static final RegistryObject<Item> CRATE = block(UnnamedModBlocks.CRATE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> DADDY = REGISTRY.register("daddy", () -> new DaddyItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
